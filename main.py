41738bb0e7dc486a9418a464805cd177f7aea3da
n-dett <124851780+n-dett@users.noreply.github.com>
2025-05-06 19:23:50 -0500

    Extract text from webpage pdfs

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+import pymupdf
 import sample_data as sd
 from functions import process_response, get_ai_response
 
 
 def main():
+    # # Load API key
+    # load_dotenv()
+    # api_key= os.environ.get("OPENAI_API_KEY")
+    #
+    # # New OpenAI instance
+    # client = OpenAI(api_key=api_key)
 
-    # Load API key
-    load_dotenv()
-    api_key= os.environ.get("OPENAI_API_KEY")
+    # Recipes folder
+    dir_path = "/Users/nataliedettmer/Library/Mobile Documents/com~apple~CloudDocs/Recipes"
 
-    # New OpenAI instance
-    client = OpenAI(api_key=api_key)
+    # Loop through all files in directory
+    for entry in os.scandir(dir_path):
+        if entry.is_file():
+            file_path = entry.path
+            print(file_path)
+
+            # Read text from PDF
+            webpage_pdf = pymupdf.open(file_path)
+            webpage_text = ""
+            for page in webpage_pdf:  # iterate the document pages
+                text = page.get_text()  # get plain text (is in UTF-8)
+                webpage_text += text
+
+            print (webpage_text, "\n\n\n\n\n")
 
 
-    pdf_text = ""
 
-    # Sends pdf text to GPT; receives extracted recipe text
-    response_text = get_ai_response(client, pdf_text)
 
-    # Clean up GPT response
-    recipe_text = process_response(response_text)
+            # # Send pdf text to GPT; receive extracted recipe text
+            # response_text = get_ai_response(client, input_pdf_text)
+            #
+            # # Clean up GPT response
+            # recipe_text = process_response(response_text)
 
 
 if __name__ == "__main__":
