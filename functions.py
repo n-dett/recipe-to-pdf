58c5dbd097a7edabf581087097a3bb82772b1504
n-dett <124851780+n-dett@users.noreply.github.com>
2025-05-06 22:07:00 -0500

    Add function to create recipe book pdf

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+import pymupdf
+import os
+from pathlib import Path
+
 def get_ai_response(client, input_string:str)->str:
     """Sends pdf text to GPT; receives extracted recipe text"""
     response = client.responses.create(
@@ -18,3 +22,31 @@ def process_response(ai_response:str)->str:
     # Remove **Recipe Title** text
     title = title.replace("**Recipe Title:**  \n", "")
     print(title)
+
+
+def create_recipe_book():
+    """ Creates recipe book PDF if it doesn't exist"""
+    recipe_book_path = Path("/Users/nataliedettmer/Library/Mobile Documents/com~apple~CloudDocs/recipe-book.pdf")
+
+    if not recipe_book_path.is_file():
+        # Create new file
+        recipe_book = pymupdf.open()
+
+        # Create title page
+        n = recipe_book.insert_page(-1,  # default insertion point
+                            text="My Recipe Book",
+                            fontsize=11,
+                            width=595,
+                            height=842,
+                            fontname="Helvetica",  # default font
+                            fontfile=None,  # any font file name
+                            color=(0, 0, 0))  # text color (RGB)
+        recipe_book.save(recipe_book_path)
+
+    else:
+        recipe_book = pymupdf.open("/Users/nataliedettmer/Library/Mobile Documents/com~apple~CloudDocs/recipe-book.pdf")
+
+    return recipe_book
+
+def new_recipe_book_page(recipe_book_path:str):
+    pass
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+from pathlib import Path
 import pymupdf
 import sample_data as sd
-from functions import process_response, get_ai_response
+from functions import process_response, get_ai_response, create_recipe_book
 
 
 def main():
@@ -17,29 +18,35 @@ def main():
     # Recipes folder
     dir_path = "/Users/nataliedettmer/Library/Mobile Documents/com~apple~CloudDocs/Recipes"
 
-    # Loop through all files in directory
-    for entry in os.scandir(dir_path):
-        if entry.is_file():
-            file_path = entry.path
-            print(file_path)
+    # Creates recipe book if necessary
+    recipe_book_pdf = create_recipe_book()
 
-            # Read text from PDF
-            webpage_pdf = pymupdf.open(file_path)
-            webpage_text = ""
-            for page in webpage_pdf:  # iterate the document pages
-                text = page.get_text()  # get plain text (is in UTF-8)
-                webpage_text += text
 
-            print (webpage_text, "\n\n\n\n\n")
+    # Process all files in directory
+    # for entry in os.scandir(dir_path):
+    #     if entry.is_file():
+    #         file_path = entry.path
+    #         print(file_path)
+    #
+    #         # Read text from PDF
+    #         webpage_pdf = pymupdf.open(file_path)
+    #         webpage_text = ""
+    #         for page in webpage_pdf:  # iterate the document pages
+    #             text = page.get_text()  # get plain text (is in UTF-8)
+    #             webpage_text += text
+    #
+    #         print (webpage_text, "\n\n\n\n\n")
 
 
 
 
             # # Send pdf text to GPT; receive extracted recipe text
-            # response_text = get_ai_response(client, input_pdf_text)
+            # response_text = get_ai_response(client, webpage text)
             #
             # # Clean up GPT response
             # recipe_text = process_response(response_text)
+
+            # Save recipe in a PDF
 
 
 if __name__ == "__main__":
